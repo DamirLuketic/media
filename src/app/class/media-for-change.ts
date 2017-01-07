1ddac407fb7ef4ca45a8d91b1cbf56dfb6ce280a
Damir <luketic.damir@gmail.com>
2017-01-07 08:46:21 +0100

    remove \media\ and add \audio\ and \video\ service

--- src/app/class/media-for-change.ts
+++
@@ -1,6 +0,0 @@
-export class MediaForChange {
-    constructor(public id: number, public user_id: number, public category_id: number, public condition_id: number,
-                public m_name: string, public m_director: string, public m_actor: string, public a_band: string,
-                public a_album: string, public year: string, public comment: string,
-    ){}
-}
--- src/app/shared/class/auth.ts
+++ src/app/shared/class/auth.ts
@@ -1,4 +1,4 @@
 export class Auth {
     constructor(public id: number, public name: string, public email: string,
-                public admin: boolean, public visible: boolean) {}
+                public admin: boolean, public image_url, public visible: boolean) {}
 }
